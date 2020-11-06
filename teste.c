a65b79b641483566136baf7df42358f8446e6511
Dawid Oliveira <dawid.oliveira@hotmail.com>
2020-11-06 17:49:54 -0300

    Initial Commit

---
+++ teste.c
@@ -0,0 +1,15 @@
+/* class main */
+void main(void){
+  int a;
+  int b;
+
+  a = 10;  /* Testando
+              comentário
+              em bloco. */
+  b = 5;
+  
+  if(a > 9){
+    a = (4 + 5) * 3 + b;
+  }
+}
+
